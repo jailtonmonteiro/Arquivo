755e990df277ccd24a9decfd420874f7fe36fbd8
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-07-08 11:34:48 -0300

    Teste inicial com função File

---
+++ Arquivo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"E:\C#\register.txt";
+            string targetPath = @"E:\C#\register2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+
+                fileInfo.CopyTo(targetPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An erro ocurred");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
